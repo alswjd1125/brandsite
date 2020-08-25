ef03f39cb106dc64644f6996e17c370412df777e
bab <eyeyaaa84@gmail.com>
2020-08-25 11:48:33 +0900

    0

---
+++ js/contact.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 11
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 5
+                }
+            },
+            messages: {
+                name: {
+                    required: "성함을 입력하세요.",
+                    minlength: "두 글자 이상을 입력해주세요."
+                },
+                subject: {
+                    required: "내용을 입력하세요.",
+                    minlength: "네 글자 이상 입력하세요."
+                },
+                number: {
+                    required: "연락처를 입력해주세요.",
+                    minlength: "열한 자리 숫자를 입력하세요."
+                },
+                email: {
+                    required: "연락처를 입력하세요.",
+                    minlength: "내용을 더 입력하세요."
+                },
+                message: {
+                    required: "내용을 입력하세요.",
+                    minlength: "내용을 더 입력하세요."
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
